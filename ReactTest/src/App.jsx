26f469e9a0e8e8176d0f12eb9c274d8884b535c7
Hohops <166859907+hohops@users.noreply.github.com>
2026-06-16 12:29:51 +0100

    phrase update

--- ReactTest/src/App.jsx
+++ ReactTest/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <h1  className="Welcome2" >to</h1>
         <h1 className="Welcome3" >My Page</h1>
         <h1 className="Main-Title"> What is this page? </h1>
-        <p className="Main-Text"> This page is a simple React application that demonstrates the use of components, styling, and JSX. It serves as a basic template for building more complex applications in the future. The page includes a welcome message and a brief description of its purpose !!</p>
+        <p className="Main-Text"> This page is a simple React application that I have been building bc I'm bored</p>
       </section>
       <Badge />
     </>
